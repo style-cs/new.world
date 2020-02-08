78cc7202be617bf46d04164bcf70abbb2dafb6d9
style-cs <clashroyalhaim11@gmail.com>
2020-02-08 04:48:59 +0300

    Updates

---
+++ precache-manifest.4fc2babb70a53fc15980f6316814635b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb6f93fb2c1f9ce33d49c394291be2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "357d49621a403fd92f6f",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "9f34a0a61b68f8f5abd6",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "357d49621a403fd92f6f",
+    "url": "./static/js/2.f19fbb6e.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.f19fbb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f34a0a61b68f8f5abd6",
+    "url": "./static/js/main.ea786ff0.chunk.js"
+  },
+  {
+    "revision": "3a1239fc2129040adce3",
+    "url": "./static/js/runtime-main.0c494397.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
